45f418293fbcf1047cfc0040a0f116dced235b55
Muhammad Thomas Fadhila Yahya <mthomasfadhilayahya@gmail.com>
2018-07-08 11:55:45 +0700

    v0.1

---
+++ TA_PCD/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace TA_PCD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbTemp = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btFindMatch = new System.Windows.Forms.Button();
+            this.btOpenImage = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btOpenTemp = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.pbResult = new System.Windows.Forms.PictureBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.numSimilarity = new System.Windows.Forms.NumericUpDown();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTemp)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
+            this.groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSimilarity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbImage);
+            this.groupBox1.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 400);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image";
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(6, 18);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(430, 376);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 3;
+            this.pbImage.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pbTemp);
+            this.groupBox2.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(461, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 179);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Template";
+            // 
+            // pbTemp
+            // 
+            this.pbTemp.Location = new System.Drawing.Point(6, 18);
+            this.pbTemp.Name = "pbTemp";
+            this.pbTemp.Size = new System.Drawing.Size(188, 155);
+            this.pbTemp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTemp.TabIndex = 3;
+            this.pbTemp.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btFindMatch);
+            this.groupBox3.Controls.Add(this.btOpenImage);
+            this.groupBox3.Location = new System.Drawing.Point(12, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(233, 61);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // btFindMatch
+            // 
+            this.btFindMatch.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btFindMatch.Location = new System.Drawing.Point(105, 12);
+            this.btFindMatch.Name = "btFindMatch";
+            this.btFindMatch.Size = new System.Drawing.Size(119, 42);
+            this.btFindMatch.TabIndex = 5;
+            this.btFindMatch.Text = "Find Match";
+            this.btFindMatch.UseVisualStyleBackColor = true;
+            this.btFindMatch.Click += new System.EventHandler(this.btFindMatch_Click);
+            // 
+            // btOpenImage
+            // 
+            this.btOpenImage.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btOpenImage.Location = new System.Drawing.Point(6, 12);
+            this.btOpenImage.Name = "btOpenImage";
+            this.btOpenImage.Size = new System.Drawing.Size(93, 42);
+            this.btOpenImage.TabIndex = 4;
+            this.btOpenImage.Text = "Open";
+            this.btOpenImage.UseVisualStyleBackColor = true;
+            this.btOpenImage.Click += new System.EventHandler(this.btOpenImage_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btOpenTemp);
+            this.groupBox4.Location = new System.Drawing.Point(461, 3);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(105, 61);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            // 
+            // btOpenTemp
+            // 
+            this.btOpenTemp.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btOpenTemp.Location = new System.Drawing.Point(6, 12);
+            this.btOpenTemp.Name = "btOpenTemp";
+            this.btOpenTemp.Size = new System.Drawing.Size(93, 42);
+            this.btOpenTemp.TabIndex = 4;
+            this.btOpenTemp.Text = "Open";
+            this.btOpenTemp.UseVisualStyleBackColor = true;
+            this.btOpenTemp.Click += new System.EventHandler(this.btOpenTemp_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.pbResult);
+            this.groupBox5.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox5.Location = new System.Drawing.Point(461, 287);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 183);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Result";
+            // 
+            // pbResult
+            // 
+            this.pbResult.Location = new System.Drawing.Point(6, 18);
+            this.pbResult.Name = "pbResult";
+            this.pbResult.Size = new System.Drawing.Size(188, 159);
+            this.pbResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResult.TabIndex = 3;
+            this.pbResult.TabStop = false;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.numSimilarity);
+            this.groupBox6.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox6.Location = new System.Drawing.Point(251, 3);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(114, 61);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Similarity";
+            // 
+            // numSimilarity
+            // 
+            this.numSimilarity.DecimalPlaces = 1;
+            this.numSimilarity.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numSimilarity.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numSimilarity.Location = new System.Drawing.Point(12, 23);
+            this.numSimilarity.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSimilarity.Name = "numSimilarity";
+            this.numSimilarity.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.numSimilarity.Size = new System.Drawing.Size(89, 26);
+            this.numSimilarity.TabIndex = 7;
+            this.numSimilarity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 482);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Deteksi Wajah Dengan Template Matching";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTemp)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).EndInit();
+            this.groupBox6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numSimilarity)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btOpenImage;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btOpenTemp;
+        private System.Windows.Forms.Button btFindMatch;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.PictureBox pbResult;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.NumericUpDown numSimilarity;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pbTemp;
+    }
+}
+
---
+++ TA_PCD/Form1.cs
@@ -0,0 +1,120 @@
+﻿/**
+ * Muhammad Thomas Fadhila Yahya 
+ * IF15Gx
+ * 15312574
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using AForge.Imaging;
+
+namespace TA_PCD
+{
+    public partial class Form1 : Form
+    {
+        Bitmap obj1, obj2, obj3, obj4, obj5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btOpenImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult ofd = openFileDialog1.ShowDialog();
+                if (ofd == DialogResult.OK)
+                {
+                    obj1 = new Bitmap(openFileDialog1.FileName);
+                    obj2 = new Bitmap(openFileDialog1.FileName);
+                    for (int x = 0; x < obj1.Width; x++)
+                    {
+                        for (int y = 0; y < obj1.Height; y++)
+                        {
+                            Color w = obj1.GetPixel(x, y);
+                            int wg = (int)(w.R + w.G + w.B) / 3;
+                            Color new_w = Color.FromArgb(wg, wg, wg);
+                            obj1.SetPixel(x, y, new_w);
+                        }
+                    }
+                    pbImage.Image = obj2;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception:" + ex.ToString());
+            }
+        }
+
+        private void btOpenTemp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult ofd = openFileDialog1.ShowDialog();
+                if (ofd == DialogResult.OK)
+                {
+                    obj3 = new Bitmap(openFileDialog1.FileName);
+                    obj4 = new Bitmap(openFileDialog1.FileName);
+                    for (int x = 0; x < obj3.Width; x++)
+                    {
+                        for (int y = 0; y < obj3.Height; y++)
+                        {
+                            Color w = obj3.GetPixel(x, y);
+                            int wg = (int)(w.R + w.G + w.B) / 3;
+                            Color new_w = Color.FromArgb(wg, wg, wg);
+                            obj3.SetPixel(x, y, new_w);
+                        }
+                    }
+                    pbTemp.Image = obj4;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception:" + ex.ToString());
+            }
+        }
+
+        private void btFindMatch_Click(object sender, EventArgs e)
+        {
+            ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching(0);
+            float sim = (float)numSimilarity.Value;
+            TemplateMatch[] matchings = tm.ProcessImage(obj1, obj3);
+            int X, Y;
+
+            Graphics g = Graphics.FromImage(obj2);
+            if (matchings[0].Similarity > sim)
+            {
+                X = matchings[0].Rectangle.X;
+                Y = matchings[0].Rectangle.Y;
+
+                g.DrawRectangle(new Pen(Color.Red, 3), X, Y, matchings[0].Rectangle.Width, matchings[0].Rectangle.Height);
+                pbImage.Image = obj2;
+                
+                obj5 = new Bitmap(matchings[0].Rectangle.Width, matchings[0].Rectangle.Height);
+                for (int x = 0; x < matchings[0].Rectangle.Width; x++)
+                {
+                    for (int y = 0; y < matchings[0].Rectangle.Height; y++)
+                    {
+                        Color w = obj2.GetPixel(x + X, y + Y);
+                        obj5.SetPixel(x, y, w);
+                    }
+                }
+                pbResult.Image = obj5;
+
+                MessageBox.Show("Kecocokan ditemukan.");
+            }
+            else
+            {
+                MessageBox.Show("Kecocokan tidak ditemukan.");
+            }
+        }
+    }
+}
